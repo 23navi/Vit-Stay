57a02e469a3a67b35749083457f4fb938b9aacf4
Navi sureka <navisureka23@gmail.com>
2022-08-04 01:11:20 +0530

    basic crud

--- app.js
+++ app.js
@@ -19,12 +19,75 @@ app.get("/",(req,res)=>{
     res.render("home.ejs");
 })
 
-
-app.get("/makecampground",async (req,res)=>{
-    const camp = new Campground({title: "FirstCamp",price:"45",description:"The first ever campground",location:"Surat"})
-    await camp.save();
-    res.send(camp)
+//show all campgrounds
+app.get("/campground",async (req,res)=>{
+    const campgrounds= await Campground.find({});
+    console.log("get/campground");
+    res.status(200).render("campground/index.ejs",{campgrounds})
 })
+
+
+//create new campground
+
+//1: get/form 
+app.get("/campground/new",(req,res)=>{
+    res.status(200).render("campground/new.ejs");
+})
+
+//2: post/form accept
+app.post("/campground",async (req,res)=>{
+    const camp = new Campground(req.body);
+    await camp.save();
+    res.status(200).redirect(`campground/${camp.id}`);
+})
+
+
+//edit campground
+//1: Edit form
+
+app.get("/campground/edit/:id",async (req,res)=>{
+    const camp= await Campground.findById(req.params.id);
+    console.log(req.params);
+    res.status(200).render("campground/edit.ejs",{camp})
+
+})
+
+app.put("/campground/:id",async (req,res)=>{
+    const camp=await Campground.findById(req.params.id);
+    camp.title=req.body.title;
+    camp.location=req.body.location;
+    await camp.save();
+    res.status(200).redirect(`/campground/${camp.id}`);
+})
+
+
+
+
+
+
+//show a particular campground
+app.get("/campground/:id",async (req,res)=>{
+    const camp = await Campground.findById(req.params.id);
+    if(camp){
+        res.status(200).render("campground/show.ejs",{camp})
+    }else{
+        console.log("error?")
+        res.status(404).send("error! no campground with id");
+    }
+    
+})
+
+
+
+
+//Delete a campground
+app.delete("/campground/:id",async (req,res)=>{
+    await Campground.findByIdAndDelete(req.params.id);
+    res.redirect("/campground");
+})
+
+
+
 
 
 app.listen(PORT,()=>{
