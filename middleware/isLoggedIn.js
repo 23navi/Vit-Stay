ed33324b94822513c2b6d8acf6df0d1ed0ecb518
Navi sureka <navisureka23@gmail.com>
2022-08-19 21:38:50 +0530

    new

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -2,7 +2,6 @@ module.exports.isLoggedIn = (req,res,next)=>{
     if(req.isAuthenticated()){
         return next()
     }else{
-        console.log("this is running");
         req.session.returnTo= req.originalUrl;
         console.log(req.session.returnTo);
         req.flash("error","You need to login first");
